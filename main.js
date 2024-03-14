12ea1cdc90e77e683645461510e1f73e88138694
Ilsa. <ilsasyeda@gmail.com>
2024-03-14 15:49:56 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var guest = [
+    "miral",
+    "sundas",
+    "urooj",
+    "alla dawas",
+    "umair",
+    "ali",
+    "amal",
+    "bilal",
+];
+console.log(guest.length);
---
+++ main.ts
@@ -0,0 +1,12 @@
+var guest = [
+    "miral",
+    "sundas",
+    "urooj",
+    "alla dawas",
+    "umair",
+    "ali",
+    "amal",
+    "bilal",
+];
+//length of items in array
+console.log(guest.length);
